5a9007ee72fbf2fd031890a221ecc32b5ed69198
Ben Aaron <ben.aaron@mixmode.ai>
2023-05-18 11:25:05 -0700

    cleanup terminal bindings and window nav

--- private_dot_config/nvim/lua/bda/remap.lua
+++ private_dot_config/nvim/lua/bda/remap.lua
@@ -1,25 +1,52 @@
 vim.g.mapleader = " "
+
+--[[ Directory View --]]
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+
+--[[ Mark navigation --]]
 vim.keymap.set("n", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("n", "K", ":m '>-2<CR>gv=gv")
 
+--[[ Scrolling --]]
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+--[[ Search Result Nav --]]
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+--[[ Hell I can't remember --]]
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+--[[ Hell I can't remember --]]
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+--[[ disable Q --]]
 vim.keymap.set("n", "Q", "<nop>")
 
+--[[ find and replace string under cursor --]]
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+--[[ Navigating Diagnostics --]]
 vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {})
 vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {})
-vim.keymap.set("n", "<leader>t", ":FloatermNew<CR>")
+
+--[[ Terminal --]]
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("n", "<leader>nt", ":terminal<CR>a")
+--[[ FloatTerm --]]
+vim.keymap.set("n", "<leader>ft", ":FloatermNew<CR>")
+
+--[[ Alt for window nav --]]
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
+
+vim.keymap.set("t", "<A-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set("t", "<A-l>", "<C-\\><C-n><C-w>l")
